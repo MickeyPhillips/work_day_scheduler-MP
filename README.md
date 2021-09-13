10dac546c4a97c8483eccb9b4c5a01789cd40e4e
Mickey Phillips <phillipsmickey41@gmail.com>
2021-09-12 22:33:48 -0500

    Progressed time functions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+## Description
+
+![_C__Users_Mickey%20Phillips_Desktop_projects_work-day-scheduler_index html(iPad Pro)](https://user-images.githubusercontent.com/83983556/133019955-a6de218b-4003-4b3a-888a-11bd4722a9f7.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,79 @@
-var currentDay = document.querySelector("#currentDay");
+var tasks = {};
+// Display the date
+var today = function() {
+    var currentDay = document.querySelector("#currentDay");
+    currentDay.textContent = moment().format("dddd, MMMM Do");
+};
+today();
 
-currentDay.textContent = moment().format("dddd, MMMM Do");
+var auditTask = function() {
+   var time = moment().format("h a");
+   notPresent(time);
+   if (time === "9 am") {
+        $("#9AM").addClass("present");
+   }
+   else if (time === "10 am") {
+        $("#10AM").addClass("present");
+   }
+   else if (time === "11 am") {
+        $("#11AM").addClass("present");
+   }
+   else if (time === "12 pm") {
+        $("#12PM").addClass("present");
+   }
+   else if (time === "1 pm") {
+        $("#1PM").addClass("present");
+   }
+   else if (time === "2 pm") {
+        $("#2PM").addClass("present");
+   }
+   else if (time === "3 pm") {
+        $("#3PM").addClass("present");
+   }
+   else if (time === "4 pm") {
+        $("#4PM").addClass("present");
+   }
+   else if (time === "5 pm") {
+        $("#5PM").addClass("present");
+   }
+
+   else {
+        $(".col-10").addClass("past");
+   }
+   
+};
+var notPresent = function(notTime) {
+    if (moment().isAfter(notTime)) {
+        $(".col-10").addClass("past");
+    }
+    else if (Math.abs(moment().diff(notTime, "hours")) <= 1) {
+        $(".col-10").addClass("future");
+    }
+    else {
+        console.log("Something wrong");
+    }
+};
+auditTask();
+
+var loadTasks = function() {
+   tasks = JSON.parse(localStorage.getItem("tasks"));
+};
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+};
+
+$(".saveBtn").on("click", "i", function() {
+    
+
+   saveTasks();
+});
+
+loadTasks();
+
+setInterval(function() {
+    $(".col-10").each(function(index, el) {
+        auditTask(el);
+   });
+   
+}, (1000 * 60) * 60);
